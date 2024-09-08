81fe0bd9e741f8f55cc68887637f11aedffa6938
vseleyshiy <122272280+vseleyshiy@users.noreply.github.com>
2024-09-08 23:11:18 +0500

    EasyLinks 2.0.0 Update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # EasyLinks
-my first python program, which was made a long time ago for my own use. now I decided to put it out for public use
+This is a program that automatically opens _all your links_ that you have specified.
+
+## Get started
+Install all the necessary dependencies from the __requirements.txt__ file into your venv folder.
+```
+pip install -r requirements.txt
+```
+
+For the program to work, you need to run the __main.py__ file in the root of the folder.
+
+After launching the application, you will need to indicate the number of links you want to add and then enter them into a special input field.
+
+Once you have entered the links, restart the program using the convenient button: you can now use this application.
+
+## Functions
+
+* You can open a specific link or open all at once.
+
+* You can delete or add links.
+
+## Contacts
+
+[developer website and donates :)](https://vseleyshiy.github.io/visitka/)
--- main.py
+++ main.py
@@ -1,70 +1,84 @@
-print('SIMPLER OPENING OF LINKS')
-print('prog made by vseleyshiy\n')
+import eel
+import webbrowser as web
 
-count = open('count.txt', 'r')
-count_read = count.read()
-if count_read != '':
+import os
+import sys
+import re
+
+eel.init('web')
+
+@eel.expose
+def count_is_empty():
+    count_read = open('count.txt', 'r')
+    
+    if count_read.read() != '':
+        count_read.close()
+        return True
+    else:
+        count_read.close()
+        return False    
+    
+    
+@eel.expose
+def add_link(link, num):
+    links = open('links.txt', 'a')
+    count = open('count.txt', 'a')
+    links.write(link + ',')
+    count.write(str(num) + ',')
+            
+    count.close()
+    links.close()
+    
+@eel.expose
+def get_links():
     file = open('links.txt', 'r')
     links = file.read().split(',')
     file.close()
-    count = open('count.txt', 'r')
-    counts = count.read().split(',')
-    count.close()
-    import webbrowser as web
-
-    def validator(func):
-        def wrapper(url):
-            if 'https://' in url:
-                func(url)
-                print('Вы открыли ', url)
-            else:
-                print('Вы ввели неверный url')
-        return wrapper
-
-    @validator
-    def open(url):
-        web.open(url)
-
-    len_counts = len(counts)
-    for el in range(len_counts):
+    
+    return links
+    
+@eel.expose
+def open_link(url):
+    web.open(url)
+    
+@eel.expose
+def open_all_links():
+    links = get_links()
+    links_length = len(links)
+    
+    for el in range(links_length):
         if links[el] == '':
             pass
         else:  
-            open(links[el])
-    
-    count.close()
-    print('Ссылки открыты, удачи <3')
-else:
-    links_count = 0
-    mess_count = 0
-    
-    print('У вас нет сохранённых ссылок. Сколько ссылок хотите добавить?')
-    try:
-        links_count = int(input('Введите число ссылок: '))
-        if links_count == 0:
-            print('Вы не можете ввести число 0')
-        elif links_count == 1:
-            print('Дальше введите корректную ссылку с протоколом https//')
-        else:
-            print('Дальше введите корректные ссылки с протоколом https//')
-    except:
-        print('То что вы набрали, не является числом')
-        links_count = int(input('Введите число ссылок: '))
-        
-    file = open('links.txt', 'a')
-    count = open('count.txt', 'w')
+            open_link(links[el])
+            
+            
+def delete_count():
+    with open('count.txt', "r") as count:
+        lines = count.readlines()
+        arr = lines[0].split(',')
+        arr.pop()
+        arr.pop()
+        count.close()
+    with open("count.txt", "w") as f:
+        for el in arr:
+            f.write(str(el) + ',')
+        f.close()
+            
+@eel.expose
+def delete_link(link):
+    with open('links.txt') as links:
+        lines = links.readlines()
+        arr = lines[0].split(',')
+        index = arr.index(str(link))
+        delete_count()
+        arr.pop()
+        arr.pop(index)
+        links.close()
+    with open("links.txt", "w") as f:
+        for el in arr:
+            f.write(el + ',')
+        f.close()
 
-    for mess_count in range(links_count):
-        mess_count += 1
-        count.write(str(mess_count) + ',')
-        links_count -= 1
-        link = input('Введите ' + str(mess_count) + ' ссылку: ')
-        file.write(link + ',')
-        
-    count.close()
-    file.close()
-    
-    print('Ссылки сохранены, перезапустите файл <3')
-    
-import time
-time.sleep(10)
+
+eel.start('index.html', size=(700, 700))
